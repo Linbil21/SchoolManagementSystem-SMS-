47283dab8f3934273eacd09fc56be3d67cebacd8
linbil24 <linbilcelestre3@gmail.com>
2026-02-14 20:21:38 +0800

    Fix Admission Sidebar Links to use correct casing (Modules) - Force Update

--- Admission/Components/Sidebar.php
+++ Admission/Components/Sidebar.php
@@ -4,7 +4,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 // Robust absolute-relative path logic
 $script_name = $_SERVER['SCRIPT_NAME'];
-$check_paths = ['/Super-admin/', '/modules/', '/Admin/', '/submodules/', '/Cashier/', '/Admission/', '/auth/', '/student/'];
+$check_paths = ['/Super-admin/', '/Modules/', '/Admin/', '/Submodules/', '/Cashier/', '/Admission/', '/auth/', '/student/', '/modules/', '/submodules/'];
 $project_base = '';
 
 foreach ($check_paths as $path) {
@@ -325,6 +325,7 @@ $email = isset($_SESSION['email']) ? $_SESSION['email'] : 'admission@sms.com';
     .sidebar-menu ul li a:hover {
         background: var(--hover-bg);
         color: var(--accent-color);
+        328:         color: var(--accent-color);
     }
 
     .icon-box {
