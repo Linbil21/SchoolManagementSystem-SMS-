9638ca0c1b1749796024adab35ab95ea6fc983c2
linbil24 <linbilcelestre3@gmail.com>
2026-02-15 22:04:01 +0800

    .

--- Super-admin/Modules/Courses.php
+++ Super-admin/Modules/Courses.php
@@ -52,7 +52,7 @@ $courses = $stmt->fetchAll();
     <title>Course Management - Super Admin</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../Assets/super-admin.css">
+    <link rel="stylesheet" href="../assets/super-admin.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
         :root {
