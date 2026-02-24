72276fc2086c9bc56c8ed76ace6a5f7b8d6ad0c9
linbil24 <linbilcelestre3@gmail.com>
2026-02-24 20:24:56 +0800

    .

--- Admission/Modules/Student-Requirements.php
+++ Admission/Modules/Student-Requirements.php
@@ -306,13 +306,7 @@ try {
             border-width: 2px;
         }
 
-        /* .student-modal-footer {
-            padding: 20px 32px;
-            border-top: 1px solid #edf2f7;
-            display: flex;
-            justify-content: flex-end;
-            background: #f8fafc;
-        } */
+       
     </style>
 </head>
 
